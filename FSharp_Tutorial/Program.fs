22aa2000a948b884db8696e4ae5af70b54000214
AriDevK <AriadneFave@hotmail.com>
2023-09-28 22:17:20 -0500

    Learn loops (while, for and foreach)

--- FSharp_Tutorial/Program.fs
+++ FSharp_Tutorial/Program.fs
@@ -1,14 +1,25 @@
-﻿// Simple if-else statement
-let age = 22
+﻿// for in loop (yeah like python)
+printfn "FOR IN LOOP"
+let list = [1;2;3;4;5]
+for i in list do
+    printf $"{i}"
 
-if age >= 18
-then printf "You can vote!"
-else printf "You can't vote!"
 
+// for to loop (like the normal for)
+printfn "\n\nFOR TO LOOP"
+for i = 1 to 10 do
+    printf $"{i}"
+    
 
-// If-else as an expression
-let canVote = if age >= 18 then true else false
-
-if canVote
-then printf "You can vote!"
-else printf "You can't vote!"
+// for to loop in reverse
+printfn "\n\nFOR TO LOOP (REVERESE)"
+for i = 10 downto 1 do
+    printf $"{i}"
+    
+    
+// while loop
+printfn "\n\nWHILE LOOP"
+let mutable i = 0
+while i < 10 do
+    printf $"{i}"
+    i <- i + 1
