f8cd0b6e9dfad3c5072bf6e758f92aab67f1590e
AriDevK <AriadneFave@hotmail.com>
2023-09-28 22:23:38 -0500

    Learn functions (on a functional lang)

--- FSharp_Tutorial/Program.fs
+++ FSharp_Tutorial/Program.fs
@@ -1,25 +1,18 @@
-﻿// for in loop (yeah like python)
-printfn "FOR IN LOOP"
-let list = [1;2;3;4;5]
-for i in list do
-    printf $"{i}"
+﻿// functions
+let add a b = a + b
 
-
-// for to loop (like the normal for)
-printfn "\n\nFOR TO LOOP"
-for i = 1 to 10 do
-    printf $"{i}"
+// function that returns a value
+let addAndMultiply a b c =
+    let sum = add a b
+    sum * c // return value
     
 
-// for to loop in reverse
-printfn "\n\nFOR TO LOOP (REVERESE)"
-for i = 10 downto 1 do
-    printf $"{i}"
+// function with defined return type and parameter types
+let convert (param: string) : int =
+    int param
     
     
-// while loop
-printfn "\n\nWHILE LOOP"
-let mutable i = 0
-while i < 10 do
-    printf $"{i}"
-    i <- i + 1
+// calling functions
+printfn $"Sum function: {add 1 2}"
+printfn $"Add and multiply function: {addAndMultiply 1 2 3}"
+printfn "Convert function: %d" (convert "123")
