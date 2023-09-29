da1e7444180c75f0ddb9fb84b27b1a9878821918
AriDevK <AriadneFave@hotmail.com>
2023-09-28 22:38:56 -0500

    Learn create lists and some functions

--- FSharp_Tutorial/Program.fs
+++ FSharp_Tutorial/Program.fs
@@ -1,29 +1,40 @@
-﻿//* Base functions
-let addTwo x = x + 2
-let multiplyByFive x = x * 5
+﻿// Create a list in line
+let numbersInLine = [1;2;3;4;5]
+
+// Create a list with multiple lines
+let numbersBlock = [
+    1
+    2
+    3
+    4
+    5
+]
+
+//** Is not possible to mix types in a list
+
+// Create a list with .. operator
+let numbersRange = [1..5] // 1,2,3,4,5
 
 
-//-> Normal way
-let addAndMultiply x =
-    let total = addTwo x
-    let result = multiplyByFive total
-    result
-    
+// ------------------------------------------
 
-//-> Composition way
-//composition is a way to combine two functions into one
-let addAndMultiplyCompose = addTwo >> multiplyByFive
+// Adding elements to a list
+// List are inmutable, so we need to create a new list with the new elements
+
+// 6,1,2,3,4,5  (Add at the beginning) [::] operator
+let newNumbers = 6 :: numbersInLine
 
 
-//-> Canalization way
-//canalization is a way to call a function and pass the result to another function
-let addAndMultiplyCanalization x =
-    x
-    |> addTwo
-    |> multiplyByFive
-    
-// also can be written as
-// x |> addTwo |> multiplyByFive
-   
+// Adding a list to a list (Concatenation) [@] operator
+// 1,2,3,4,5,6,7,8,9
+let newNumbers2 = numbersInLine @ [6;7;8;9]
 
 
+// ------------------------------------------
+
+// List properties
+newNumbers2.Head // First element of the list (1)
+newNumbers2.Tail // List without the first element (2,3,4,5,6,7,8,9)
+newNumbers2.Length // Length of the list (9)
+newNumbers2.IsEmpty // Check if the list is empty (false)
+newNumbers2.Item 2 // Get the element in the position 2 (3)
